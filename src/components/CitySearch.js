00b749aae315ac24360304d4b5e5052aca5aba75
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-05-03 20:57:56 -0400

    Create integration test for CitySearch

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,21 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const CitySearch = ({ allLocations }) => {
+const CitySearch = ({ allLocations, setCurrentCity }) => {
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [query, setQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
 
-  // 1) Obtains the current value of the input field
-  // 2) Based on the value, function will filter the "allLocations" array
-  // 3) It will then set the "Query" local state to whateever the input value is
-  // 4) Finally will set the "suggestions" local state to the filtered locations array
+  useEffect(() => {
+    setSuggestions(allLocations);
+  }, [`${allLocations}`]);
+
+  // 1) Obtains current value of input field 2) Based on the value, function filters "allLocations" array 3) It will then set the "Query" local state to whatever input value is 4) Will set "suggestions" local state to filtered locations array
   const handleInputChanged = (event) => {
     const value = event.target.value;
-    const filteredLocations = allLocations
-      ? allLocations.filter((location) => {
-          return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-        })
-      : [];
+    const filteredLocations = allLocations ? allLocations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    })
+  : [];
 
     setQuery(value);
     setSuggestions(filteredLocations);
@@ -24,7 +24,8 @@ const CitySearch = ({ allLocations }) => {
   const handleItemClicked = (event) => {
     const value = event.target.textContent;
     setQuery(value);
-    setShowSuggestions(false); // to hide the list
+    setShowSuggestions(false);
+    setCurrentCity(value);
   };
 
   return (
