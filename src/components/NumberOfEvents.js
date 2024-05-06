53e948578049aed022feb34090531dd01a73a25c
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-05-06 17:21:33 -0400

    Add "NumberOfEvents" text

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,5 +1,3 @@
-// import { useState } from "react";
-
 const NumberOfEvents = ({ setCurrentNOE }) => {
 
   const handleInputChanged = (event) => {
@@ -9,6 +7,7 @@ const NumberOfEvents = ({ setCurrentNOE }) => {
 
   return (
     <div id="number-of-events">
+      <label htmlFor="number-of-events-input">Number of Events: </label>
       <input 
         type="text" 
         className="number-of-events-input" 
