a01562ceeb24621c8c08c826e38574a4fb554012
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-05-06 16:17:59 -0400

    Create integration test to render number of events inputted by user

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -23,36 +23,45 @@ describe("<App /> component", () => {
 });
 
 describe("<App /> integration", () => {
-  // This test ensures that the number of rendered events in the UI equals the number of events located in "Berlin, Germany".
+
   test("renders a list of events matching the city selected by the user", async () => {
-    // userEvent is set and the App component and its DOM are mocked
     const user = userEvent.setup();
     const AppComponent = render(<App />);
     const AppDOM = AppComponent.container.firstChild;
 
-    // A reference to the CitySearch component root DOM node is initialized, then a query is performed to find the city input text box in it.
     const CitySearchDOM = AppDOM.querySelector("#city-search");
     const CitySearchInput = within(CitySearchDOM).queryByRole("textbox");
 
-    // Simulates typing "Berlin" in the city textbox and then clicking on the list item that contains "Berlin, Germany".
     await user.type(CitySearchInput, "Berlin");
     const berlinSuggestionItem = within(CitySearchDOM).queryByText("Berlin, Germany");
     await user.click(berlinSuggestionItem);
 
-    // This code queries #event-list (the EventList component root node DOM), and finds what Event list item is rendered inside it.
     const EventListDOM = AppDOM.querySelector("#event-list");
     const allRenderedEventItems = within(EventListDOM).queryAllByRole("listitem");
 
-    // Gets a list of all events from the mock data that are located in "Berlin, Germany".
     const allEvents = await getEvents();
     const berlinEvents = allEvents.filter(event => event.location === "Berlin, Germany");
 
-    // Comparing the number of events located in "Berlin, Germany" with the array of rendered Event list items, epecting them to have the same length.
     expect(allRenderedEventItems.length).toBe(berlinEvents.length);
 
-    // Loops over the filtered event list items with "allRenderedEventItems". With this, you can then make sure that all of the items contain the text "Berlin, Germany".
     allRenderedEventItems.forEach(event => {
       expect(event.textContent).toContain("Berlin, Germany");
     });
+  });
+
+  test("renders the number of events inputted by the user", async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+    
+    const NumberOfEventsDOM = AppDOM.querySelector("#number-of-events");
+    const NumberOfEventsInput = within(NumberOfEventsDOM).queryByRole("textbox");
+    
+    await user.type(NumberOfEventsInput, "{backspace}{backspace}10");
+    
+    const EventListDOM = AppDOM.querySelector("#event-list");
+    const allRenderedEventItems = within(EventListDOM).queryAllByRole("listitem");
+    
+    expect(allRenderedEventItems.length).toEqual(10);
   });
 });
