a8d61f7e700a36bfbba35703a8229265e87c16f1
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-05-10 11:41:18 -0400

    Create end-to-end tests for Feature 1

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -3,6 +3,7 @@ import puppeteer from "puppeteer";
 describe("show/hide an event details", () => {
   let browser;
   let page;
+
   beforeAll(async () => {
     browser = await puppeteer.launch({
       headless: true, // set to false to turn "headless mode" off (to visually see the tests in Chromium browser)
@@ -13,7 +14,6 @@ describe("show/hide an event details", () => {
     await page.goto("http://localhost:3000/");
     await page.waitForSelector(".event");
   });
-
   afterAll(() => {
     browser.close();
   });
@@ -33,5 +33,42 @@ describe("show/hide an event details", () => {
     await page.click(".event .details-btn");
     const eventDetails = await page.$(".event .details");
     expect(eventDetails).toBeNull();
+  });
+});
+
+describe("filter events by city", () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: true, // set to false to turn "headless mode" off (to visually see the tests in Chromium browser)
+      slowMo: 250, // slow down by 250ms,
+      timeout: 0 // removes any puppeteer/browser timeout limitations (not the same as the timeout of jest)
+    });
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+    await page.waitForSelector(".event");
+  });
+  afterAll(() => {
+    browser.close();
+  });
+
+  test("When user has not searched for a city, show upcoming events from all cities.", async () => {
+    const eventList = await page.$("#event-list");
+    expect(eventList).not.toBeNull();
+  });
+
+  test("User should see a list of suggestions when they search for a city.", async () => {
+    await page.type(".city", "Berlin, Germany");
+    const suggestionListItems = await page.$(".city");
+    expect(suggestionListItems.length).not.toBeNull();
+  });
+
+  test("User can select a city from the suggested list.", async () => {
+    await page.waitForSelector(".suggestions");
+    await page.click(".suggestions li:first-child");
+    const cityInputValue = await page.$eval(".city", input => input.value);
+    expect(cityInputValue).toBe("Berlin, Germany");
   });
 });
