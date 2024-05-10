d614181ad421522476438838e711063a8fee505c
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-05-10 11:39:00 -0400

    Update to double quote syntax

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -8,14 +8,14 @@ const feature = loadFeature("./src/features/specifyNumberOfEvents.feature");
 defineFeature(feature, test => {
   
   // Feature 3, Scenario 1
-  test('When user has not specified a number, 32 events are shown by default.', ({ given, when, then }) => {
+  test("When user has not specified a number, 32 events are shown by default.", ({ given, when, then }) => {
     let AppComponent;
 
-    given('the user has not typed a specific number of events to be displayed', () => {
+    given("the user has not typed a specific number of events to be displayed", () => {
       AppComponent = render(<App />);
     });
 
-    when('the user is viewing the list of events', () => {
+    when("the user is viewing the list of events", () => {
       const AppDOM = AppComponent.container.firstChild;
       const EventListDOM = AppDOM.querySelector("#event-list");
       expect(EventListDOM).toBeInTheDocument();
@@ -32,10 +32,10 @@ defineFeature(feature, test => {
   });
 
   // Feature 3, Scenario 2
-  test('User can change the number of events displayed.', ({ given, when, then }) => {
+  test("User can change the number of events displayed.", ({ given, when, then }) => {
     let AppComponent;
 
-    given('the user typed a specific number of events to be displayed', async () => {
+    given("the user typed a specific number of events to be displayed", async () => {
       const user = userEvent.setup();
       AppComponent = render(<App />);
       const AppDOM = AppComponent.container.firstChild;
@@ -44,13 +44,13 @@ defineFeature(feature, test => {
       await user.type(numberInput, "{backspace}{backspace}10");
     });
 
-    when('the user is viewing the list of events', () => {
+    when("the user is viewing the list of events", () => {
       const AppDOM = AppComponent.container.firstChild;
       const EventListDOM = AppDOM.querySelector("#event-list");
       expect(EventListDOM).toBeInTheDocument();
     });
 
-    then('the exact number of events that the user specified should be displayed', () => {
+    then("the exact number of events that the user specified should be displayed", () => {
       const AppDOM = AppComponent.container.firstChild;
       const EventListDOM = AppDOM.querySelector("#event-list");
       const allRenderedItems = within(EventListDOM).queryAllByRole("listitem");
