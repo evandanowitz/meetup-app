4732b5d454cde2d03a2fc0d12f9599b7a942cef7
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-05-22 15:49:16 -0400

    ResizeObserver code to fix test errors caused by adding charts

--- src/setupTests.js
+++ src/setupTests.js
@@ -19,3 +19,17 @@ console.error = (...args) => {
 };
 
 jest.setTimeout(30000);
+
+const { ResizeObserver } = window;
+beforeEach(() => {
+  delete window.ResizeObserver;
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+});
+afterEach(() => {
+  window.ResizeObserver = ResizeObserver;
+  jest.restoreAllMocks();
+});
